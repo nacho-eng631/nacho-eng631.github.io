ba515f8d8dd653d031c69ebe4e9086f3b2751025
nacho-eng631 <dukelee2005@gmail.com>
2026-04-13 12:20:19 +0800

    Add files via upload

---
+++ week08/week08-5a.c
@@ -0,0 +1,9 @@
+//week08-5a
+#include <stdio.h>
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	if (N>2) printf("%d", N-2);
+	else printf("%d", 2-N);
+}
---
+++ week08/week08-5c.c
@@ -0,0 +1,8 @@
+//week08-5c
+#include <stdio.h>
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	printf("%d",(N-1)%7);
+}
