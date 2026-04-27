66fede537bb4cc62e2e9465c0fa3cac92f75b598
nacho-eng631 <dukelee2005@gmail.com>
2026-04-27 11:10:02 +0800

    Delete week08 directory

--- week08/week08-5c.c
+++
@@ -1,8 +0,0 @@
-//week08-5c
-#include <stdio.h>
-int main()
-{
-	int N;
-	scanf("%d", &N);
-	printf("%d",(N-1)%7);
-}
--- week08/werk08-3a.c
+++
@@ -1,8 +0,0 @@
-//werk08-3a.cpp
-#include <stdio.h>
-int main()
-{
-	char a,b,c,d,e;
-	scanf("%c%c%c%c%c", &a, &b, &c, &d, &e);
-	printf("%c   %c   %c   %c   %c", a, b, c, d, e);
-}
